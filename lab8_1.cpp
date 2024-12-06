a1ef26c896b5a4635efdd66011c4513c804318e1
ErrkkSkyseer <lolplayer78876@gmail.com>
2024-12-06 15:14:20 +0700

    "Lab8 Completed!!!"

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -6,12 +6,32 @@ int main()
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+
+  int n = rank == 'S' ? 5 : rank == 'A' ? 4
+                        : rank == 'B'   ? 3
+                        : rank == 'C'   ? 2
+                        : rank == 'D'   ? 1
+                                        : -1;
+
+  if (n > 0)
+  {
+    if (n > 1)
+    {
+      if (n > 2)
+      {
+        if (n > 3)
+        {
+          if (n > 4)
+          {
+            cout << "You have received Super Ultra Rare Unit!!!\n";
+          }
+          cout << "You have received 5 gems.\n";
+        }
+        cout << "You have received 1 gems.\n";
+      }
+      cout << "You have received 2000 coins.\n";
+    }
+    cout << "You have received very KAK items.\n";
   }
   return 0;
 }
